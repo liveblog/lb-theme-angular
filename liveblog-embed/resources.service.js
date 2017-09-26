b9cf1f8df512631f83975f0ec4466d71598ecbcf
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2017-09-26 21:37:13 +0300

    fix: on client_modules not `syndication_in` fied is pre embedded.

--- liveblog-embed/resources.service.js
+++ liveblog-embed/resources.service.js
@@ -159,7 +159,7 @@
             }
             return obj;
         }
-        return $resource(config.api_host + 'api/client_blogs/:blogId/posts?embedded={"syndication_in":1}', {blogId: config.blog._id}, {
+        return $resource(config.api_host + 'api/client_blogs/:blogId/posts', {blogId: config.blog._id}, {
             get: {
                 transformResponse: function(posts) {
                     // decode json
